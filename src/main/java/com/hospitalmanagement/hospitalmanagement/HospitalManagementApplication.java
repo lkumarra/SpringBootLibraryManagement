7d34f7c3e8850e89cf1cd0bf843a2df1531fe92c
Lavendra Kumar Rajput <60038605+lkumarra@users.noreply.github.com>
2023-03-11 23:22:01 +0530

    Added Swagger Configuration

--- src/main/java/com/hospitalmanagement/hospitalmanagement/HospitalManagementApplication.java
+++ src/main/java/com/hospitalmanagement/hospitalmanagement/HospitalManagementApplication.java
@@ -1,9 +1,12 @@
 package com.hospitalmanagement.hospitalmanagement;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2; 
 
 @SpringBootApplication
+@EnableSwagger2
 public class HospitalManagementApplication {
 
 	public static void main(String[] args) {
