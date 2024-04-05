97a0c32c10e46b3f2cdf54f75cb2360394b53782
lkumarrajput <Lavendra.rajput@monotype.com>
2024-04-05 13:26:20 +0530

    Updated Data

--- src/main/java/com/librarymanagement/librarymanagement/LibraryManagementApplication.java
+++ src/main/java/com/librarymanagement/librarymanagement/LibraryManagementApplication.java
@@ -3,8 +3,9 @@ package com.librarymanagement.librarymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2; 
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class LibraryManagementApplication {
